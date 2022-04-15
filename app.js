9898bd6714a3d626e4a5bd7212ad223fcd6628ba
praveenaThota97 <thotapraveena97@gmail.com>
2022-04-15 13:42:10 +0530

    Updated Code

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 const addNumbers = (...nums) => {
-  
-    return nums.reduce((total,num)=>total+num,0);
+//   for(var i=0;i<nums.length;i++){
+//       console.log(nums[i]);
+//       if(nums[i]<0){
+//           console.log("Negative are not allowed");
+//       }
+//   }
+var numberArray=[];
+var strArray= Array.from(nums[0]);
+ //console.log(strArray);
+ for(var i=0;i<strArray.length;i++){
+     try{
+        if(!isNaN(parseInt(strArray[i])) && parseInt(strArray[i])<0){
+            throw new Error ("Negatives not allowed"+strArray[i]);
+            break;
+         }
+         if(!isNaN(parseInt(strArray[i])) && parseInt(strArray[i])>0){
+            numberArray.push(parseInt(strArray[i]));
+            
+         }
+     }finally{
+
+     }
+     
+    
+ }
+ console.log(numberArray+"numberArray");
+    return numberArray.reduce((total,num)=>total+num,0);
+    
+    
 }; 
 
 module.exports = addNumbers;
--- app.test.js
+++ app.test.js
@@ -3,22 +3,30 @@ const addNumbers = require('./app');
 describe('addNumbers',()=>{
     //Test-1 - If the value is empty, then output should be zero
     it('empty string returm sum as Zero',()=>{
-        expect(addNumbers()).toBe(0);
+        expect(addNumbers("")).toBe(0);
     })
     //Test-2 - Should return sum of two numbers
     it('return sum of entered nmbers',()=>{
-        expect(addNumbers(1,2)).toBe(3);
+        expect(addNumbers("1,2")).toBe(3);
     })
     //Test-3 - Should return solo output, if only one value is given
     it('Should return solo output',()=>{
-        expect(addNumbers(5)).toBe(5);
+        expect(addNumbers("5")).toBe(5);
     })
     //Test-4 - Should return sum of 3 numbers
     it('Should return sum of entered numbers',()=>{
-        expect(addNumbers(5,5,5)).toBe(15);
+        expect(addNumbers("5,5,5")).toBe(15);
     })
     //Test-5 - Should add any number of arguments
     it('Should add any number of inputs',()=>{
-        expect(addNumbers(5,5,5,5,5,5)).toBe(30);
+        expect(addNumbers("5,5,5,5,5,5")).toBe(30);
+    })
+     //Test-6 - Should allow \n between input numbers
+     it('Should allow \\n between input numbers',()=>{
+        expect(addNumbers("5\n5")).toBe(10);
+    })
+    //Test-7 - Should not allow negative numbers to add
+    it('Should not allow negative numbers to add',()=>{
+        expect(addNumbers("-1,-2,5")).toThrow("Negatives are not allowed");
     })
 })
